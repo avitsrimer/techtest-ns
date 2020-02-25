61046ec166dae5589c2aead2d53bd02ce70a2b33
Aleksandr Vits-Rimer <vic.rimer@gmail.com>
2020-02-25 12:48:41 +0700

    Added in tree repo methods for get root and child.

--- src/Repository/TreeRepository.php
+++ src/Repository/TreeRepository.php
@@ -6,5 +6,14 @@ use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
 
 class TreeRepository extends NestedTreeRepository
 {
+    public function getArrayRootNodes()
+    {
+        return $this->getRootNodesQuery()->getArrayResult();
+    }
+
+    public function getArrayOfChildsById($id)
+    {
+        return $this->getChildrenQuery($this->find($id), true)->getArrayResult();
+    }
     
 }
